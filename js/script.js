483c36032c27d5b4e5c9f9c26a2f36dcb39f6efd
Oleg <oleggalyanovskij@gmail.com>
2023-11-22 22:09:02 +0200

    add new file js

--- js/script.js
+++ js/script.js
@@ -1,78 +1,25 @@
-"use strict";
-let number = 5;
-const LeftBorderWidth = 1;
+/* Задание на урок:
 
-number = 10;
-console.log (number);
-let userName = 'John';
-let userNumber = 25;
-    userNumber = 24;
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-const obj = {
-  name: 'Oleg',
-  age: 45,
-  marriade: true
-}
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-console.log(obj.age);
-console.log(obj['name']);
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-let arr = ['orange.jpg', 6, 'logo.png', 'name.webp'];
-console.log(arr[1]);
+Проверить, чтобы все работало без ошибок в консоли */
 
-const storeDescription = {
-  budget: 10000,
-  employees: ['Oleg', 'Vika', 'Olga'],
-  products: {
-      orange: 1000,
-      apple: 500
-  },
-  open: true
-}
+'use strict';
 
-// alert('Hello');
-// const resalt = confirm('Ти тут?');
-// console.log(resalt);
-// const answer = prompt('Тобу вже 18?', '18'); //+ перед prompt зробить результат числом. А так завжди відповідь користувача стрічка
-// console.log(typeof(answer), answer);
-
-// const answers = [];
-// answers[0] = prompt ('Ваше імя?', '');
-// answers[1] = prompt ('Ваше прізвище?', '');
-// answers[2] = prompt ('Скільки років?', '');
-
-// console.log(answers);
-// document.write(answers); // перезаписує дані сторінки браузера
-
-const category = 'toys';
-console.log('https://mydomen.com/' + category);
-console.log(`https://mydomen.com/${category}/5`);
-
-const user = 'Oleg';
-console.log(`Hello, ${user}`);
-console.log(4 + +'5'); //+ перед 5 перетворює стрічку 5 в число 5 =>9
-
-let incr = 10,
-    decr = 10;
-incr++; //постфіксний запис
-decr--;
-console.log(incr);
-console.log(decr);
-++incr; //префіксний запис
---decr;
-console.log(incr);
-console.log(decr);
-
-console.log(incr++); // спочатку виводяться старі значення а потім відбувається дія
-console.log(decr--);
-
-console.log(++incr); //спочатку відбувається дія, а потім виводиться значення
-console.log(--decr);
-
-let a = 10, b = 10;
-// console.log(++a, --b);
-console.log(a%b); //залишок від ділення => 0
-
-const isChecked = true,
-      isClosed = false;
-console.log(isChecked || !isClosed); //знак оклику перетворює неправду в правду
