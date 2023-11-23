bdda7d934cd78dd2b5d99210a41184bdd67ef613
Oleg <oleggalyanovskij@gmail.com>
2023-11-23 14:17:31 +0200

    lessons if && || and example

--- js/script.js
+++ js/script.js
@@ -1,35 +1,46 @@
 "use strict";
 
-if (4 == 4) {
-  console.log('Правда');
+// if (4 == 4) {
+//   console.log('Правда');
+// } else {
+//   console.log('Обман');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//   console.log('error');
+// } else if (num > 100) {
+//   console.log('Багато');
+// } else {
+//   console.log('OK!');
+// }
+
+// (num === 50) ? console.log('Ok!') : console.log('error'); // тернарний оператор - три аргумента
+
+// const my = 50;
+// switch (my) {
+//   case 49:
+//     console.log('error');
+//     break;
+//   case 100:
+//     console.log('error');
+//     break;
+//   case 50:
+//     console.log('it is good');
+//     break;
+//   default:
+//     console.log('no resault');
+//     break;
+// }
+
+const hamburger = 1;
+const cola = 1;
+const fries = 1;
+const nuggets = 1;
+
+if (hamburger || fries || cola || nuggets ) {
+  console.log('Good');
 } else {
-  console.log('Обман');
-}
-
-const num = 50;
-
-if (num < 49) {
-  console.log('error');
-} else if (num > 100) {
-  console.log('Багато');
-} else {
-  console.log('OK!');
-}
-
-(num === 50) ? console.log('Ok!') : console.log('error'); // тернарний оператор - три аргумента
-
-const my = 50;
-switch (my) {
-  case 49:
-    console.log('error');
-    break;
-  case 100:
-    console.log('error');
-    break;
-  case 50:
-    console.log('it is good');
-    break;
-  default:
-    console.log('no resault');
-    break;
+  console.log('bad');
 }
