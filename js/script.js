31998b95b55ae7f7ea1a2e400cde484d7ef82da8
Oleg <oleggalyanovskij@gmail.com>
2023-11-23 14:19:08 +0200

    lessons if && || and example+

--- js/script.js
+++ js/script.js
@@ -34,13 +34,55 @@
 //     break;
 // }
 
-const hamburger = 1;
-const cola = 1;
-const fries = 1;
-const nuggets = 1;
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
 
-if (hamburger || fries || cola || nuggets ) {
-  console.log('Good');
-} else {
-  console.log('bad');
+// if (hamburger ===3 && cola ===2 || fries ===3 && nuggets ) {
+//   console.log('Good');
+// } else {
+//   console.log('bad');
+// }
+// console.log(hamburger || fries || cola || nuggets);
+
+
+
+console.log( NaN || 2 || undefined );
+console.log( NaN && 2 && undefined );
+console.log( 1 && 2 && 3 );
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( NaN || null || !3 || undefined || 5);
+console.log( NaN || null && !3 && undefined || 5);
+console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
 }
