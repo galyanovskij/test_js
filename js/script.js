c8a1b03d380ac46d4589b6ec0c12cbf385dca456
Oleg <oleggalyanovskij@gmail.com>
2023-11-26 22:57:04 +0200

    29-8 return Math.max

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function calculateVolumeAndArea(a) {
-  if (a != null && !isNaN(a)) {
-    console.log(`Объем куба: ${a*a*a}, площадь всей поверхности: ${a*a*6}`);
-  }
-   console.log('При вычислении произошла ошибка');
-}
-
-calculateVolumeAndArea(5);
