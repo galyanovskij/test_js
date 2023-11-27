31a2face3a69a3ed1874a94428e0847ccb32fb52
Oleg <oleggalyanovskij@gmail.com>
2023-11-27 16:42:40 +0200

    trim and collback

--- js/script_29_8_9_Math_max_fibonachi.js
+++ js/script_29_8_9_Math_max_fibonachi.js
@@ -114,13 +114,67 @@ function findMaxNumber(a, b ,c, d) {
 findMaxNumber(250);
 findMaxNumber(1, 5, '6', '10');
 
+// числа Фибоначчи, где первые два числа равны 0 и 1, а каждое последующее число равно сумме двух предыдущих чисел.
+
+// Создайте функцию, которая будет принимать в себя один аргумент-целое положительное число. Она должна возвращать строку, в которой будут через пробел выведены числа Фибоначчи. Причем, их количество должно быть равно переданному аргументу.
+
+// Если переданный аргумент не число - вернуть пустую строку. Решать без применения рекурсии.
+// Пример:
+// fib(4) => ''0 1 1 2"
+// fib(7) => ''0 1 1 2 3 5 8"
+// fib('7') => ''"
+// fib(1) => "0"
+// fib(0) => ''"
 
 function fib(k) {
-  const arr[];
-  for (i=2 ; i < k; i++) {
-    arr[i] = arr[i-1] + arr[i-2];
-    
+  if (typeof(k) !== 'number' || k <= 0 || !Number.isInteger(k)) {
+    console.log('');
+    return('');
+  } else {
+    switch (k) {
+      case 0: 
+      console.log('');
+      return('');
+      case 1:
+        console.log('0');
+        return('0');
+    }
+    const arr = [];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (let i=2 ; i < k; i++) {
+      arr[i] = arr[i-1] + arr[i-2];
+    }
+     console.log(arr.join(' '));
+     return(arr.join(' '));
   }
-   console.log(arr); 
 }
-fib(10);
+fib(0 1 1 2 3 5);
+
+відповідь 
+function fib(num) {
+  if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+      return "";
+  }
+
+  let result = '';
+  let first = 0;
+  let second = 1;
+
+  for (let i = 0; i < num; i++) {
+      if (i + 1 === num) {
+          result += `${first}`;
+          // Без пробела в конце
+      } else {
+          result += `${first} `;
+      }
+
+      let third = first + second;
+      first = second;
+      second = third;
+  }
+
+  return result;
+}
+
+fib(5)
