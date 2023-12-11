0775af1f91f55bcfe4be37d9371278d77902238e
Oleg <oleggalyanovskij@gmail.com>
2023-12-11 22:21:35 +0200

    arr join push return if

---
+++ js/arr_join_push_return.js
@@ -0,0 +1,75 @@
+У вас есть список учеников, которые хотят поиграть в игру:
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+Но команд может быть только 3 по 3 человека. Напишите функцию sortStudentsByGroups, которая принимает в себя массив строк.
+Внутри она сначала сортирует имена по алфавиту. Затем распределяет учеников по 3 человека в 3 группы по алфавитному порядку. Эти группы должны быть массивами. Как итог, функция возвращает новый массив с тремя командами и строкой как 4й элемент.
+Пример:
+sortStudentsByGroups(students)  =>
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: Takesi'
+]
+Если убрать одно студента из списка, то результат будет:
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: -'
+]
+А если добавить одного, то:
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: Takesi, Somebody'
+]
+То есть, меняется содержимое строки. Все оставшиеся ученики попадают туда.
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+  arr.sort(); // сортує за алфавітом як стрічки
+  let arrOne = [], arrTwo = [], arrThree = [], arrRest = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i <3 ) {
+      arrOne[i] = arr[i];
+    } else if (i <6){
+      arrTwo[i-3] = arr[i];
+    } else if (i < 9){
+      arrThree[i-6] = arr[i];
+    }else {
+      arrRest[i-9] =arr[i];
+    } 
+  }
+  console.log(arrOne);
+  console.log(arrTwo);
+  console.log(arrThree);
+  console.log(arrOne,arrTwo,arrThree, `Оставшиеся студенты: ${arrRest.length === 0 ? '-' : arrRest.join(', ')}`);
+  return [arrOne,arrTwo,arrThree, `Оставшиеся студенты: ${arrRest.length === 0 ? '-' : arrRest.join(', ')}`]
+}
+
+sortStudentsByGroups(students);
+
+// Код Івана
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}
+
+sortStudentsByGroups(students);
