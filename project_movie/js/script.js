53859d559e9d5dd4c1fef1c58d2c6b182ab6d9e5
Oleg <oleggalyanovskij@gmail.com>
2023-12-13 18:46:26 +0200

    document - forEach - sort - innerHTML

--- project_movie/js/script.js
+++ project_movie/js/script.js
@@ -24,17 +24,36 @@ const movieDB = {
     ]
 };
 
-const promo__adv = document.querySelector('.promo__adv'),
+// const promo__adv = document.querySelector('.promo__adv'),
+ const adv_img = document.querySelectorAll('.promo__adv img'),
     promo__genre = document.querySelector('.promo__genre'),
     promo__bg = document.querySelector('.promo__bg'),
     list_movies = document.querySelector('.promo__interactive-list'),
     movies = list_movies.querySelectorAll('li');
-promo__adv.remove();
+// promo__adv.remove();
+adv_img.forEach(item =>{
+    item.remove();
+})
+// adv_img.forEach(function (item) {
+//     item.remove();
+// })
+
 promo__genre.innerHTML = 'Драма'; 
 promo__bg.style.background = "url('img/bg.jpg')";
 movies[1].style.backgroundColor = 'red';
 movieDB.movies.sort();
 movieDB.movies.forEach((item, i) =>{
     movies[i].innerHTML = ` ${i + 1}. ${movieDB.movies[i]}`;
-//   console.log(`Любимый жанр ${i + 1} - это ${item}`);
 });  
+
+// варіант ві Івана - він видаляє блок з переліком філььмів і записує туди нові елементи
+list_movies.innerHTML = ''; //записуючи пустоту ми видаляємо код html з блока list_movies
+
+movieDB.movies.forEach((film, i) => {
+    list_movies.innerHTML += `
+    <li class="promo__interactive-item">${i+1}. ${film}
+        <div class="delete"></div>
+    </li>
+    
+    `
+});
